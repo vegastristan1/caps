39459d3b15d78749e729b4e4a644ffb4b4299a93
Tristan Vegas <vegastristan1@gmail.com>
2025-01-05 15:40:48 +0800

    db connection

---
+++ imp/db.php
@@ -0,0 +1,15 @@
+<?php
+// db.php: Database connection setup
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "enrollment";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
